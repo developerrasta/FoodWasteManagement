bee2de21161c927d26c50bbb68aa4d4bd0757fb0
jayalakshmi97 <jayalaskhmiajayakumar97@gmail.com>
2019-11-27 15:27:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodwastemanagement/Register1.java
@@ -0,0 +1,91 @@
+package com.example.foodwastemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class Register1 extends AppCompatActivity {
+
+    Button next1;
+    EditText fname,lname,hname,hno,street,district;
+    RadioGroup radioGroup;
+    SharedPreferences sharedPreferences;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register1);
+
+        next1 = findViewById(R.id.next1);
+
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        hname = findViewById(R.id.hname);
+        hno = findViewById(R.id.hno);
+        street = findViewById(R.id.street);
+        district = findViewById(R.id.district);
+
+        radioGroup = findViewById(R.id.radiogroup);
+
+        radioGroup.clearCheck();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton)group.findViewById(checkedId);
+            }
+        });
+
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                sharedPreferences = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE); //MyPREFERENCES="MyPrefs"
+
+
+                if(!(
+                        fname.getText().toString().isEmpty() ||
+                                lname.getText().toString().isEmpty() ||
+                                hname.getText().toString().isEmpty() ||
+                                hno.getText().toString().isEmpty()   ||
+                                street.getText().toString().isEmpty()||
+                                district.getText().toString().isEmpty() ||
+                                selectedId== -1
+
+                ))
+                {
+                    RadioButton radioButton = (RadioButton)radioGroup.findViewById(selectedId);
+
+                    //Toast.makeText(Register1.this, radioButton.getText(),Toast.LENGTH_SHORT).show();
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("FirstName", fname.getText().toString()); //FirstName=key
+                    editor.putString("LastName", lname.getText().toString()); //FirstName=key
+                    editor.putString("HouseName", hname.getText().toString()); //FirstName=key
+                    editor.putString("HouseNumber", hno.getText().toString()); //FirstName=key
+                    editor.putString("Street", street.getText().toString()); //FirstName=key
+                    editor.putString("District", district.getText().toString()); //FirstName=key
+                    editor.putString("Gender", radioButton.getText().toString()); //FirstName=key
+                    editor.apply();
+
+                }
+                else
+                {
+                    Toast.makeText(Register1.this,"Incorrect Values",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
