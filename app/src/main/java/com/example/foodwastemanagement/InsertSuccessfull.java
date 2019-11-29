c6c256d44f5f9ce5dec080ed9f5c467a4b5b65eb
jayalakshmi97 <jayalaskhmiajayakumar97@gmail.com>
2019-11-29 14:54:41 +0530

    Update

--- app/src/main/java/com/example/foodwastemanagement/InsertSuccessfull.java
+++ app/src/main/java/com/example/foodwastemanagement/InsertSuccessfull.java
@@ -4,6 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class InsertSuccessfull extends AppCompatActivity {
 
@@ -46,55 +62,71 @@ public class InsertSuccessfull extends AppCompatActivity {
 
 
 
-//        StringRequest stringRequest = new StringRequest(Request.Method.POST,"https://lilac-wing.000webhostapp.com/PaymentApplication/Register.php",
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//
-//                        //If we are getting success from server
-//
-//                        Toast.makeText(Profile.this,response,Toast.LENGTH_LONG).show();
-//
-//
-//
-//                        try {
-//                            JSONArray jsonArray=new JSONArray(response);
-//                            for(int i=0;i<jsonArray.length();i++){
-//                                JSONObject json_obj = jsonArray.getJSONObject(i);
-//
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        //error handling
-//                    }
-//
-//                }){
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String,String> params = new HashMap<>();
-//                //Adding parameters to request
-//
-//                params.put("name",name.getText().toString()); //name=$_POST['name'];
-//                params.put("phonenum",phno.getText().toString());
-//                params.put("username",uname.getText().toString());
-//                params.put("upiid",upi.getText().toString());
-//                params.put("password",pword.getText().toString());
-//
-//                //returning parameter
-//                return params;
-//            }
-//        };
-//
-//
-//        //Adding the string request to the queue
-//        RequestQueue requestQueue = Volley.newRequestQueue(Profile.this);
-//        requestQueue.add(stringRequest);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,"https://lilac-wing.000webhostapp.com/FoodWasteManagement/Register.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //If we are getting success from server
+
+                        Toast.makeText(InsertSuccessfull.this,response,Toast.LENGTH_LONG).show();
+
+
+
+                        try {
+                            JSONArray jsonArray=new JSONArray(response);
+                            for(int i=0;i<jsonArray.length();i++){
+                                JSONObject json_obj = jsonArray.getJSONObject(i);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //error handling
+                    }
+
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                //Adding parameters to request
+
+                params.put("firstname",FirstName); //name=$_POST['name'];
+                params.put("lastname",LastName);
+                params.put("gender",Gender);
+                params.put("mobileno",MobileNo);
+                params.put("username",UserName);
+                params.put("password",Password);
+                params.put("email",Email);
+                params.put("housename",HouseName);
+                params.put("houseno",HouseNo);
+                params.put("street",Street);
+                params.put("district",District);
+                params.put("catname",CateringName);
+                params.put("catbuildno",CateringBuildingNo);
+                params.put("catstreet",CateringStreet);
+                params.put("catdistrict",CateringDistrict);
+                params.put("restname",RestName);
+                params.put("restbuildno",RestBuildingNo);
+                params.put("reststreet",RestStreet);
+                params.put("restdistrict",RestDistrict);
+
+
+                //returning parameter
+                return params;
+            }
+        };
+
+
+        //Adding the string request to the queue
+        RequestQueue requestQueue = Volley.newRequestQueue(InsertSuccessfull.this);
+        requestQueue.add(stringRequest);
+
     }
 }
